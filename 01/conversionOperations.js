ff8ba0c35abf5d1f41e5aa30c4dc791083f1facd
anubhavXLR8 <143119952+anubhavXLR8@users.noreply.github.com>
2023-08-25 20:06:03 +0000

    conersionOperation

---
+++ 01/conversionOperations.js
@@ -0,0 +1,24 @@
+/*let score="33"
+
+console.log(typeof score)
+console.log(typeof(score)) */
+
+/*let toNumber=Number(score);
+console.log(typeof(toNumber))
+console.log(typeof toNumber)
+console.log(toNumber)
+score="33ad"
+toNumber=Number(score);
+console.log(toNumber)*/ //this will give Nan Which also a type of datatype
+
+//conerssion results
+//"33" > 33
+//"33ad" > Nan
+
+let number=1
+console.log(number);
+
+let toBoolean= Boolean(number)
+console.log(toBoolean);
+console.log(typeof toBoolean);
+
