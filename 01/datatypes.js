5e8c84a8516205ea3f6487b87a020152c4675555
anubhavXLR8 <143119952+anubhavXLR8@users.noreply.github.com>
2023-08-25 19:15:05 +0000

    datatypes

---
+++ 01/datatypes.js
@@ -0,0 +1,19 @@
+"use strict" //treats all JS code as newer version
+// alert(3+3) // wont workbecause we are using nodejs not browser
+
+//dataTypes (Primitive)
+//number= 2to the power 53
+//bigInt
+//string=" "
+//boolean=True/false
+//null= standalone value // temp example from server //type is object
+//undefined= value is not defined yet //type is undefined
+//symbol= uses for uniqueness used in react and figma tools
+
+//datatype(Non-primitive)
+//object
+
+let age=25
+console.log(typeof age)
+
+
