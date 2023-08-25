cf6467bc4b0a9e4b3df1be8a5a8a19265e570823
anubhavXLR8 <143119952+anubhavXLR8@users.noreply.github.com>
2023-08-25 18:50:36 +0000

    done

---
+++ 01/variables.js
@@ -0,0 +1,12 @@
+const accountID= 12345
+let mail="canubahv@google.com"
+var password="canubhav"
+accountcity="Delhi"
+let accountState;
+
+// accountID=32123  //can be done, its contsnt
+
+console.table([accountID, mail, password, accountcity, accountState]);
+
+/*dont use var due to issues of block scope or functional scope*/
+
